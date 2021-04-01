200b9dc01260a34e1f1aef527ad9cb2472090109
Crisco849 <SecondSlasher123@Gmail.com>
2021-03-31 23:02:10 -0500

    Merged in pause prefab and script. WORKING DEMO.

--- Assets/Scripts/Player/Pause.cs
+++ Assets/Scripts/Player/Pause.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
+
+    public GameObject PauseMenu;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +22,12 @@ public class Pause : MonoBehaviour
         {
             if (Time.timeScale == 1)
             {
+                PauseMenu.SetActive(true);
                 Time.timeScale = 0;
             }
             else
             {
+                PauseMenu.SetActive(false);
                 Time.timeScale = 1;
             }
         }
