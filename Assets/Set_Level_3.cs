7d0f24f287329bce48be3cd208c3ca862b347c9d
Crisco849 <SecondSlasher123@Gmail.com>
2021-04-23 13:26:03 -0500

    Healthbar, level selection menu, transition bugs.

---
+++ Assets/Set_Level_3.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Set_Level_3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("CurrentLevel", 3);        
+    }
+
+}
